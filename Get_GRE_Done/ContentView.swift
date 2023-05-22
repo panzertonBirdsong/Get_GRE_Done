7d3dc1013573d5ab630f29ad74ffe52afc4f721b
panzertonBirdsong <Panzerton_Birdsong@outlook.com>
2023-05-22 20:57:46 +0800

    Initial Commit

---
+++ Get_GRE_Done/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Get_GRE_Done
+//
+//  Created by Birdsong 潘 on 5/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Get_GRE_Done/Get_GRE_DoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Get_GRE_DoneApp.swift
+//  Get_GRE_Done
+//
+//  Created by Birdsong 潘 on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Get_GRE_DoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
